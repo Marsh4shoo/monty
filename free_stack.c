af2d43ce3926f8fc8f7f4ec1ab6ed62f8c02da6c
Marsh4shoo <marshal.wjnr@gmail.com>
2023-08-26 14:10:09 -0700

    Updated

--- free_stack.c
+++ free_stack.c
@@ -1,18 +1,38 @@
 #include "monty.h"
-/**
-* free_stack - free doubly linked list
-* @head: head of the stack
-*/
-void free_stack(stack_t *head)
-{
-	stack_t *aux;
 
-	aux = head;
-	while (head)
-	{
-		aux = head->next;
-		free(head);
-		head = aux;
-	}
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    bus_t bus = {file, NULL, NULL, 0};
+    stack_t *stack = NULL;
+    unsigned int counter = 0;
+    char *content = NULL;
+    size_t size = 0;
+    ssize_t read_line;
+
+    while ((read_line = getline(&content, &size, file)) != -1) {
+        bus.content = content;
+        counter++;
+
+        if (read_line > 0) {
+            execute(content, &stack, counter, file);
+        }
+
+        free(content);
+        content = NULL;
+    }
+
+    free_stack(stack);
+    fclose(file);
+
+    return EXIT_SUCCESS;
 }
-
